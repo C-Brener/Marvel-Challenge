ec87879caa0f780248f6eac1952ce0b0be790c84
brenermr <Caique.Brener-ext@ab-Inbev.com>
2022-09-09 12:36:49 -0300

    test api

--- app/src/main/java/com/caiquebrener/marvelchallenge/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/caiquebrener/marvelchallenge/ui/viewmodel/SharedViewModel.kt
@@ -1,24 +1,25 @@
-package com.caiquebrener.marvelchallenge.ui
+package com.caiquebrener.marvelchallenge.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.caiquebrener.marvelchallenge.data.api.ApiService
 import com.caiquebrener.marvelchallenge.data.dto.getcomics.ComicDataWrapper
+import com.caiquebrener.marvelchallenge.domain.usecase.getcomics.GetComicsUsecase
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SharedViewModel : ViewModel() {
+class SharedViewModel(private val useCase: GetComicsUsecase) : ViewModel() {
     private val _teste = MutableLiveData<List<ComicDataWrapper>>()
     var teste: LiveData<List<ComicDataWrapper>> = _teste
 
     private val _name = MutableLiveData("")
     var name: LiveData<String> = _name
 
-    fun getCharacters() {
-        ApiService.service.listCharacters().enqueue(object : Callback<ComicDataWrapper> {
+
+    fun getCharacters(person: String) {
+        useCase.getComics(person = person).enqueue(object : Callback<ComicDataWrapper> {
             override fun onResponse(
                 call: Call<ComicDataWrapper>,
                 response: Response<ComicDataWrapper>
