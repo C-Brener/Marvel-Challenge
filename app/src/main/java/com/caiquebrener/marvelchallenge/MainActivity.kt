51cb1cd4daf05f6dd11462bf8d66422876643bc9
brenermr <Caique.Brener-ext@ab-Inbev.com>
2022-09-09 13:18:51 -0300

    remove test

--- app/src/main/java/com/caiquebrener/marvelchallenge/MainActivity.kt
+++ app/src/main/java/com/caiquebrener/marvelchallenge/MainActivity.kt
@@ -3,22 +3,15 @@ package com.caiquebrener.marvelchallenge
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.caiquebrener.marvelchallenge.databinding.ActivityMainBinding
-import com.caiquebrener.marvelchallenge.ui.viewmodel.SharedViewModel
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: SharedViewModel by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        viewModel.getCharacters("Iron Man")
-        viewModel.name.observe(this) {
-            binding.teste.text = it
-        }
 
     }
 
