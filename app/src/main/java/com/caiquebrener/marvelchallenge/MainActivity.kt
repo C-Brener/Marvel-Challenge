06dd97531bec2a46fd223b769f64b1782f20cf7d
brenermr <Caique.Brener-ext@ab-Inbev.com>
2022-09-09 22:54:01 -0300

    finish the navigation

--- app/src/main/java/com/caiquebrener/marvelchallenge/MainActivity.kt
+++ app/src/main/java/com/caiquebrener/marvelchallenge/MainActivity.kt
@@ -2,7 +2,11 @@ package com.caiquebrener.marvelchallenge
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.caiquebrener.marvelchallenge.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,8 +16,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupBottomNavMenu(navController)
     }
 
 
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav?.setupWithNavController(navController)
+    }
 }
